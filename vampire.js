f974f478e8b2d29de9629378aa09c92b80e6e6ec
Wisara8 <srandall845@gmail.com>
2018-07-06 23:07:06 +0000

    complete

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let levelsOfVampires = 0;
+    let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      levelsOfVampires++;
+    }
+
+    return levelsOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
